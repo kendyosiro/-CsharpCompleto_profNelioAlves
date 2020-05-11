876d5e7057e43e34b79de8be0cc86e31faf41059
Kendy Osiro <kendyosiro@gmail.com>
2020-05-11 12:18:10 -0300

    cap 208 introducao a delegates

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Secao16_ExpressoesLambda_Delegates_LINQ.Services;
+using System;
+
+namespace Secao16_ExpressoesLambda_Delegates_LINQ
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Services/CalculationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secao16_ExpressoesLambda_Delegates_LINQ.Services
+{
+    class CalculationService
+    {
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Max(double a, double b)
+        {
+            return (a > b) ? a : b;
+        }
+
+        public static double Square(double a)
+        {
+            return a * a;
+        }
+    }
+}
