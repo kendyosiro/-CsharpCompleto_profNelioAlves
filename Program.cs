ff47d425099071f9e87bb2382f512597a9cf792e
Kendy Osiro <kendyosiro@gmail.com>
2020-05-11 13:13:46 -0300

    210. Predicate (exemplo com RemoveAll

--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
-﻿using Secao16_ExpressoesLambda_Delegates_LINQ.Services;
+﻿using Secao16_ExpressoesLambda_Delegates_LINQ.Entities;
+using Secao16_ExpressoesLambda_Delegates_LINQ.Services;
+using System.Collections.Generic;
 
 namespace Secao16_ExpressoesLambda_Delegates_LINQ
 {
     class Program
     {
-        delegate void BinaryNumericOperation(double n1, double n2);
         static void Main(string[] args)
         {
-            double a = 10;
-            double b = 12;
+            Product p1 = new Product("TV", 1200.00);
+            Product p2 = new Product("Mesa", 400.00);
+            Product p3 = new Product("Geladeira", 2300.00);
+            Product p4 = new Product("Cadeira", 390.00);
 
-            BinaryNumericOperation op = CalculationService.ShowSum;
-            op += CalculationService.ShowMax;
+            List<Product> produtos = new List<Product>();
+            produtos.Add(p1);
+            produtos.Add(p2);
+            produtos.Add(p3);
+            produtos.Add(p4);
 
-            op.Invoke(a, b);
+            //Somente este comando resolveria:
+            //produtos.RemoveAll(x => x.Price < 1000.00);
+
+            produtos.RemoveAll(TestProduct);
+
+            foreach (Product product in produtos)
+            {
+                System.Console.WriteLine(product.ToString());
+            }
+        }
+
+        static bool TestProduct(Product p)
+        {
+            return p.Price < 1000.00;
         }
     }
 }
