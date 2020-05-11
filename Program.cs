422f252a3415633e86a5e9c64c041234f6a59b40
Kendy Osiro <kendyosiro@gmail.com>
2020-05-11 15:41:23 -0300

    213. Introdução ao LINQ

--- Program.cs
+++ Program.cs
@@ -7,45 +7,26 @@ using System.Linq;
 namespace Secao16_ExpressoesLambda_Delegates_LINQ
 {
 
-    //Fazer um programa que, a partir de uma lista de produtos, gere
-    //uma nova lista contendo os nomes dos produtos em caixa alta.
+    //Utilizando LINQ para obter os números pares de uma lista
+    //e esses números são multiplicados por 10
+
     class Program
     {
         static void Main(string[] args)
         {
-            List<Product> produtos = new List<Product>();
-            produtos.Add(new Product("TV", 1200.00));
-            produtos.Add(new Product("Mesa", 400.00));
-            produtos.Add(new Product("Geladeira", 2300.00));
-            produtos.Add(new Product("Cadeira", 390.00));
+            //1-specify a data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
 
-            //1-utilizando Select passando função como argumento
-            //List<string> results = produtos.Select(NameUpper).ToList();
+            //Define a query expression:
+            IEnumerable<int> list = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
 
-            //2-criando uma Func que recebe um Product e retorna um string e passa no select o nome da func:
-            //Func<Product, string> func = NameUpper;
-            //List<string> results = produtos.Select(func).ToList();
-
-            //3-criando uma Func utilizando expressão lambda:
-            //Func<Product, string> func = p => p.Name.ToUpper();
-            //List<string> results = produtos.Select(func).ToList();
-
-            //se eu colocar chaves na funcao, tenho que colocar a palavra return:
-            // Func<Product, string> func = p => { return p.Name.ToUpper(); };
-            // List<string> results = produtos.Select(func).ToList();
-
-            //a forma mais fácil é utiliza a expressão lambda dentro do select:
-            List<string> results = produtos.Select(p => p.Name.ToUpper()).ToList();
-
-            foreach (string s in results)
+            //Executing the query defined:
+            foreach (int number in list)
             {
-                System.Console.WriteLine(s);
+                Console.WriteLine(number);
             }
-        }
-
-        static string NameUpper(Product p)
-        {
-            return p.Name.ToUpper();
         }
     }
 }
